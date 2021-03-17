bf366a1d7fa5ec3459dd3f24d53554e523a11d17
Agcitw <80104490+Agcitw@users.noreply.github.com>
2021-03-17 13:56:52 +0300

    corrected binary operations

--- Second/Program.cs
+++ Second/Program.cs
@@ -5,28 +5,24 @@ namespace Second
 {
 	internal static class Program
 	{
-		private static readonly int[][] Sub =
+		private static readonly byte[] Sub =
 		{
-			new[] {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
-			new[] {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
-			new[] {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
-			new[] {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
+			14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7
 		};
 
 		public static void Main()
 		{
-			Console.WriteLine(Substitute(120, Sub));
+			Console.WriteLine(Substitute(8, Sub)); 
+			  //dec: 8 -> bin: 0000_0000_0000_1000 ->
+			 //ind: 0, 0, 0, 8 -> Sub[ind]: 14, 14, 14, 3 ->
+			//bin: 1110_1110_1110_0011 -> dec: 61155
 		}
 		
-		private static uint Substitute(uint value, IReadOnlyList<int[]> sub)
+		private static uint Substitute(uint value, IReadOnlyList<byte> sub)
 		{
 			uint result = 0;
 			for (var i = 0; i < 4; i++)
-			{
-				var index = (byte)(value >> (4 * i) & 0x0f);
-				var sBlock = sub[i][index];
-				result |= (uint)sBlock << (4 * i);
-			}
+				result |= (uint)sub[(byte)(value >> (4 * i) & 0x0f)] << (4 * i);
 			return result;
 		}
 	}
