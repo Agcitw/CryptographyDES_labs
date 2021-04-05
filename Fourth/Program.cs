0b844cae6832dcaae736558aabeb5e11be862ca2
Agcitw <80104490+Agcitw@users.noreply.github.com>
2021-04-05 11:18:26 +0300

    Fourth completed

--- Fourth/Program.cs
+++ Fourth/Program.cs
@@ -1,10 +1,90 @@
-﻿namespace Fourth
+﻿// 4. Реализуйте алгоритмы сети Фейстеля (шифрование и дешифрование):
+// - для языков, поддерживающих ООП: в виде класса FeistelNet с
+// 	абстрактными функциями для генерации подключей и шифрующего
+// преобразования (примените паттерн проектирования “Шаблонный
+// метод”);
+
+using System;
+
+namespace Fourth
 {
 	internal static class Program
 	{
 		public static void Main()
 		{
-			
+			var test = new TestFeistelNet(100, 200, 7);
+			test.Show();
+			test.Encrypt();
+			test.Show();
+			test.Decrypt();
+			test.Show();
+		}
+	}
+
+	internal abstract class FeistelNet
+	{
+		public abstract int Left { get; set; }
+		public abstract int Right { get; set; }
+		public abstract int Rounds { get; set; }
+		public abstract Func<int, int, int> SecretFunc { get; set; }
+		public abstract void Encrypt();
+		public abstract void Decrypt();
+	}
+
+	internal sealed class TestFeistelNet : FeistelNet
+	{
+		public override int Left { get; set; }
+		public override int Right { get; set; }
+		public override int Rounds { get; set; }
+		public override Func<int, int, int> SecretFunc { get; set; } 
+			= (i, j) => (i + j) % 256;
+
+		public TestFeistelNet(int left, int right, int rounds)
+		{
+			Left = left;
+			Right = right;
+			Rounds = rounds;
+		}
+
+		public override void Encrypt()
+		{
+			var round = 1;
+			for (var i = 0; i < Rounds; i++)
+			{
+				if (i < Rounds - 1)
+				{
+					var temp = Left;
+					Left = Right ^ SecretFunc(Left, round);
+					Right = temp;
+				}
+				else
+				{
+					Right ^= SecretFunc(Left, round);
+				}
+				round += 1;
+			}
+		}
+		public override void Decrypt()
+		{
+			var round = Rounds;
+			for (var i = 0; i < Rounds; i++)
+			{
+				if (i < Rounds - 1)
+				{
+					var temp = Left;
+					Left = Right ^ SecretFunc(Left, round);
+					Right = temp;
+				}
+				else
+				{
+					Right ^= SecretFunc(Left, round);
+				}
+				round -= 1;
+			}
+		}
+		public void Show()
+		{
+			Console.WriteLine(Left + " " + Right);
 		}
 	}
 }
--- Third/Program.cs
+++ Third/Program.cs
@@ -1,30 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// 2. Реализовать функцию substitute, принимающую целочисленное
+// 	значение n и правило замены группы из 4 бит на другую группу из 4 бит
+// 	(в виде массива sub, где sub[i] есть результат замены значения i).
+// 	Результатом функции должно быть значение n, в которой каждая группа
+// 	из 4 бит (0-3 биты, 4-7 биты, …) заменена в соответствии с переданным
+// правилом.
+// 3. Модифицируйте код из задания 2 таким образом, чтобы правило
+// 	замены передавалось на вход функции в виде функции обратного
+// вызова.
+
+using System;
 
 namespace Third
 {
 	internal static class Program
 	{
-		private delegate uint CallbackFoo(uint value, IReadOnlyList<byte> sub);
-		private static readonly byte[] Sub =
-		{
-			14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7
-		};
+		private static readonly Func<byte, byte> CallBackFunc = i => Sub[i];
+		private static readonly byte[] Sub = 
+			{ 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 };
 
 		public static void Main()
 		{
-			CallbackFoo callbackFoo = Substitute;
-			Core(callbackFoo);
+			Console.WriteLine(Substitute(8, CallBackFunc));
 		}
-
-		private static void Core(CallbackFoo callbackFoo) =>
-			Console.WriteLine(callbackFoo(8, Sub));
 		
-		private static uint Substitute(uint value, IReadOnlyList<byte> sub)
+		private static uint Substitute(uint value, Func<byte, byte> callback)
 		{
 			uint result = 0;
 			for (var i = 0; i < 4; i++)
-				result |= (uint)sub[(byte)(value >> (4 * i) & 0x0f)] << (4 * i);
+				result |= (uint)callback((byte)(value >> (4 * i) & 0x0f)) << (4 * i);
 			return result;
 		}
 	}
